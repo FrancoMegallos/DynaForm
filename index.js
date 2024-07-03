4bc95ffd13a065e56312d5220d5864b30913c2b6
Charles <j.canedo.527294@umindanao.edu.com>
2024-07-03 10:15:38 +0800

    add public folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
+app.use(express.static("public"));
 app.get("*/*", router);
 
 // Start the server
