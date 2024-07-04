d6b8e3b54f78c6fb2614d4eb7568998e01684ebe
Charles <j.canedo.527294@umindanao.edu.com>
2024-07-04 10:08:26 +0800

    Schema Changes

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ import express from "express";
 import router from "./src/routes/routes.js";
 import dbRouter from "./src/routes/dbRoutes.js"; // api routes for db bridge
 import mongoose from "mongoose";
-=======
-import { configDotenv } from "dotenv";
 
+import { configDotenv } from "dotenv";
 
 configDotenv();
 const app = express();
-
 
 app.set("view engine", "ejs"); // Set the view engine to ejs
 app.set("views", "src/views"); // Set the views directory1
@@ -24,24 +22,24 @@ app.get("*/*", router);
 app.use("/db", dbRouter);
 
 // Database Connection with mongoDB
+/*
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		// Listen for request only after connecting to MongoDB
-		app.listen(process.env.PORT, (error) => {
-			if (!error) {
-				console.log(
-					"Server is connected to MongoDB & running on port",
-					process.env.PORT
-				);
-			} else {
-				console.log(`Error ${error}`);
-			}
-		});
-	})
-	.catch((error) => console.log(error));
-
-
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for request only after connecting to MongoDB
+    app.listen(process.env.PORT, (error) => {
+      if (!error) {
+        console.log(
+          "Server is connected to MongoDB & running on port",
+          process.env.PORT
+        );
+      } else {
+        console.log(`Error ${error}`);
+      }
+    });
+  })
+  .catch((error) => console.log(error));
+*/
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
